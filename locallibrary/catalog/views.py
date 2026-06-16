e72a873dfa8a560364e06a413a4adaf102440b22
Sofia Sineglazova <sofia@Sofias-MacBook-Air.local>
2026-06-16 17:21:06 +0300

    added pagination

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -35,7 +35,7 @@ def index(request):
 
 class BookListView(generic.ListView):
     model = Book
-    # paginate_by = 10
+    paginate_by = 10
     # context_object_name = 'book_list'   # your own name for the list as a template variable
     # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
     # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
