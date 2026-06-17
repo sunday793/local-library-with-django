1d3bd351cb64ed9c373fda703e56bdb3034be0cb
Sofia Sineglazova <sofia@Sofias-MacBook-Air.local>
2026-06-17 10:34:53 +0300

    added session for counting visits of an user

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -19,6 +19,11 @@ def index(request):
     num_genres = Genre.objects.count()
 
     specific_books = Book.objects.filter(title__icontains='the').count()
+
+    # Number of visits to this view, as counted in the session variable
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
     
     context = {
         'num_books': num_books,
@@ -27,6 +32,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'specific_books': specific_books,
+        'num_visits': num_visits,
     }
     
     # Render the HTML template index.html with the data in the context variable
