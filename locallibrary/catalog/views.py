a29eb2ef3af035687598d5c03c35d386b9da506e
Sofia Sineglazova <sofia@Sofias-MacBook-Air.local>
2026-09-03 14:57:47 +0300

    added search by book title

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -41,19 +41,17 @@ def index(request):
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
-    # context_object_name = 'book_list'   # your own name for the list as a template variable
-    # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
-    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
 
-    # def get_queryset(self):
-        # return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    def get_queryset(self):
+        query = self.request.GET.get('q', '')
+        if query:
+            return Book.objects.filter(title__icontains = query)
+        return Book.objects.all()
 
-    # def get_context_data(self, **kwargs):
-    #     # Call the base implementation first to get the context
-    #     context = super(BookListView, self).get_context_data(**kwargs)
-    #     # Create any data and add it to the context
-    #     context['some_data'] = 'This is just some data'
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.request.GET.get('q', '')
+        return context
 
 class BookDetailView(generic.DetailView):
     model = Book
